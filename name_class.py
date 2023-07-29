7fa011eea8be83778c42479d9e961513260e7ae7
Melih Gergin <102462150+MelihGergin@users.noreply.github.com>
2023-07-29 23:40:04 +0300

    Create name_class.py

---
+++ name_class.py
@@ -0,0 +1,5 @@
+#I described class and saved it. Then I imported it to result.py program.
+class names():
+    a = input("What is your name      : ")
+    b = input("What is your surname   : ")
+    print("Welcome", a, b, "!")
