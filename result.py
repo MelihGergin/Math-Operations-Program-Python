c4fa512bb9e86b4c00caf000cde6eef379d5b7ad
Melih Gergin <102462150+MelihGergin@users.noreply.github.com>
2023-07-29 23:39:39 +0300

    Create Result.py

---
+++ result.py
@@ -0,0 +1,8 @@
+import name_class
+import math_operations_class
+
+name = name_class.names()
+result = math_operations_class.operations()
+
+print("Result: ", result.c)
+print("Have a good day", name.a, "!")
